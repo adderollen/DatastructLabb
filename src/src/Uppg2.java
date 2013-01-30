32f59bb840a5922d97e288930e83c539ce909a8e
Robin Hammarang <Robin.Hammarang@gmail.com>
2013-01-30 08:20:44 +0100

    Uppgift 2 done

--- src/src/Uppg2.java
+++ src/src/Uppg2.java
@@ -1,33 +1,38 @@
-import java.util.*;
+/**
+ * A class that calculates the square root of a given number. 
+ * @author Robin Hammaräng
+ * @author Andreas Rolén
+ * @group 35
+ */
 
 public class Uppg2 {
-	
-	/*
-	 * Jävligt oklart vad den här funktionen faktiskt ska göra. 
-	 * "BinarySqrt's uppgift är bara att göra rimlighetskontroll av indata och anropa den interna
-	 *	rekursiva hjälpfunktionen" står det i Labb-PM.
+
+	/**
+	 * binarySqrt returns the square root of the given sqr with precision eps.
+	 * @param sqr >= 1
+	 * @param eps > 0
+	 * @return Root
 	 */
 	public static double binarySqrt(double sqr, double eps) {
-		double result = 0;
 		if(sqr < 1){
-			throw new NumberFormatException("Sqr must be >= than 1.");
+			throw new NumberFormatException("sqr must be >= than 1.");
+		} else if( eps <= 0){
+			throw new NumberFormatException("eps must be > than 0.");
 		} else {
-			result = help(sqr, eps, 1, sqr); //The root is somewhere between 1 and sqr. (lower / upper limit).
+			return help(sqr, eps, 1, sqr); 
 		}
-		return result;
 	}
 	
 	
 	/**
-	 * Help function for binarySqrt 
-	 * to actually do the calculations.
+	 * Help function for binarySqrt to actually do the calculations.
 	 * @param lowerLimit, upperLimit: the square root is between these two numbers
 	 * @param sqr, number to calculate square root from.
 	 * @param eps, error margin.
 	 * @return the square root of the given sqr.
 	 */
 	private static double help(double sqr, double eps, double low, double high){
-		double mid = (high + low) / 2; //Value in the middle of lowerLimit and upperLimit. 
+		double mid = (high + low) / 2; //Value in the middle of low and high. 
 		double result = mid * mid;
 		if((Math.abs(result-sqr)) < eps){
 			return mid;
